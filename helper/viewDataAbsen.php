65aeb70be5488bbe46064c470ff7bb212b680f9c
ahmad rendi <ahmadrendiajah@gmail.com>
2024-01-25 23:41:48 +0800

    fetch: menampilkan data absen oleh admin

---
+++ helper/viewDataAbsen.php
@@ -0,0 +1,53 @@
+
+<?php
+
+class viewDataAbsen {
+    private $no;
+    private $stb;
+    private $nama;
+    private $kelas;
+    private $status;
+
+
+    public function setNo($no){
+        $this->no = $no;
+    }
+
+    public function getNo(){
+        return $this->no;
+    }
+
+    public function setStb($stb){
+        $this->stb = $stb;
+    }
+
+    public function getStb(){
+        return $this->stb;
+    }
+
+    public function setNama($nama){
+        $this->nama = $nama;
+    }
+
+    public function getNama(){
+        return $this->nama;
+    }
+
+    public function setKelas($kelas){
+        $this->kelas = $kelas;
+    }
+
+    public function getKelas(){
+        return $this->kelas;
+    }
+
+    public function setStatus($status){
+        $this->status = $status;
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+}
+
+?>
